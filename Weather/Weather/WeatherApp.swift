c3670d82f6b111f0f6d1c15a4b777a67196b340d
CJiu01 <jiwoo020801@gmail.com>
2024-09-15 18:17:53 +0900

    setting: Create Project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 최지우 on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
