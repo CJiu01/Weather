3a533ae3165f14109f4d49575b822816a7540e4d
CJiu01 <jiwoo020801@gmail.com>
2024-11-11 18:23:58 +0900

    feat: 최상단에서 store 생성

--- Weather/Weather/WeatherApp.swift
+++ Weather/Weather/WeatherApp.swift
@@ -5,13 +5,20 @@
 //  Created by 최지우 on 9/15/24.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct WeatherApp: App {
+    // 최상단에서 static let 변수로 store 생성
+    static let store = Store(initialState: MainTemperatureFeature.State()) {
+        MainTemperatureFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: WeatherApp.store)
         }
     }
 }
